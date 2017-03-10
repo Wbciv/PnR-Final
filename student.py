ec8b88b36d307b8c8cdaa3e948112945af5eabd4
clevelandw <clevelandw@gilmour.org>
2017-03-10 11:14:51 -0500

    changed sensor distance

--- student.py
+++ student.py
@@ -63,7 +63,7 @@ class GoPiggy(pigo.Pigo):
     def sweep(self):
         for x in range (20,160,2):
                 self.servo(x)
-                if self.dist() < 30:
+                if self.dist() < 50:
                     print("AAAHHHHH")
                     break
 
